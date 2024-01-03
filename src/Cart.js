f105678fcaa19d5f1a77ace37dd60a7da234d538
Benjamin <benjamin.boss@icloud.com>
2024-01-03 05:58:31 -0500

    added total cost of cart

--- src/Cart.js
+++ src/Cart.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 const Cart = ({ updateOrder, removeFromCart, lineItems, cart, products })=> {
+  function sumCart () {
+    let sum = 0;
+    {
+      lineItems.filter((lineItem) => {return lineItem.order_id === cart.id}).map(lineItem => {
+          const product = products.find(product => product.id === lineItem.product_id) || {};
+          return (
+            sum += product.price * lineItem.quantity
+          )
+      })
+    }
+    return sum / 100;
+  }
+  
   return (
     <div>
       <h2>Cart</h2>
@@ -20,9 +33,14 @@ const Cart = ({ updateOrder, removeFromCart, lineItems, cart, products })=> {
       </ul>
       {
         lineItems.filter((lineItem) => {return lineItem.order_id === cart.id }).length ? 
-          <button onClick={()=> {
-          updateOrder({...cart, is_cart: false });
-        }}>Create Order</button>
+          <div>
+            <span>Total Cost: ${sumCart().toFixed(2)} </span>
+            <div>
+              <button onClick={()=> {
+              updateOrder({...cart, is_cart: false });
+              }}>Create Order</button>
+            </div>
+        </div>
         : 
         <div> Add some items to your cart.</div>
       }
